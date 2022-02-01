435384101e314b751ecf6c55096e154bb668435a
PaolaAndreaGA <paga0209@gmial.com>
2022-01-31 22:58:30 -0500

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
-""" Python3 REST API example EXPORT to csv"""
+""" script to export data in the CSV format. """
 import csv
 import requests
 import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    id_employee = sys.argv[1]
-    url_user = "https://jsonplaceholder.typicode.com/users/" + id_employee
-    res = requests.get(url_user).json()
-    username = res.get("username")
-    req = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' +
-        (id_employee) + '/todos')
-    with open("{}.csv".format(sys.argv[1]), "w") as file_csv:
-        writer = csv.writer(file_csv, quoting=csv.QUOTE_ALL)
-        for task in req.json():
-            writer.writerow([id_employee, username,
-                             task.get("completed"), task.get("title")])
+    userid = sys.argv[1]
+    user = '{}users/{}'.format(url, userid)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
+
+    todos = '{}todos?userId={}'.format(url, userid)
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        l_task.append([userid,
+                       name,
+                       task.get('completed'),
+                       task.get('title')])
+
+    filename = '{}.csv'.format(userid)
+    with open(filename, mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file,
+                                     delimiter=',',
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_ALL)
+        for task in l_task:
+            employee_writer.writerow(task)
