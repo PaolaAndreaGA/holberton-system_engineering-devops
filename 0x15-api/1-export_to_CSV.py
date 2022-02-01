8fe62ff2ca8ff713676fece125ab3501a9c43176
Paola Andrea García Altamirano <85592476+PaolaAndreaGA@users.noreply.github.com>
2022-01-31 22:31:56 -0500

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,16 +4,18 @@ Uses https://jsonplaceholder.typicode.com along with an employee ID to
 return information about the employee's todo list progress
 """
 
-import csv
-import requests
-from sys import argv
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
+    import csv
+    import requests
+    import sys
+
+    uid = sys.argv[1]
+    usr = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                       .format(uid))
+    name = usr.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
     file = uid + '.csv'
     with open(file, mode='w') as f:
         write = csv.writer(f, delimiter=',', quotechar='"',
